451f007645a622c515a081b83f298742c88c6edd
sushantsy12sy12-ui <aceraspire5121@gmail.com>
2026-05-03 23:05:38 +0530

    Customers page

--- models/userModel.js
+++ models/userModel.js
@@ -1,15 +1,55 @@
 import mongoose from "mongoose";
- const userSchema=new mongoose.Schema({
-    name:{type:String,required:true},
-    phoneNumber:{type:String,required:true,unique:true},
-    email:{type:String,required:true,unique:true},
-    password:{type:String,required:true},
-    role:{type:String,enum:["user","admin"],default:"user"},
-    isVerified:{type:Boolean,default:"false"}
- },
- {
-    timestamps:true
- })
 
- export const User=mongoose.model("User",userSchema) // named export so we have to name named import
- 
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+
+    phoneNumber: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+
+    password: {
+      type: String,
+      required: true,
+      select: false
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user"
+    },
+
+    city: {
+      type: String,
+      default: "N/A"
+    },
+
+    tier: {
+      type: String,
+      enum: ["Bronze", "Silver", "Gold", "Platinum"],
+      default: "Bronze"
+    },
+
+    isVerified: {
+      type: Boolean,
+      default: false
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+export const User = mongoose.model("User", userSchema);
