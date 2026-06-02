8eb7ba61846650b9d55005654f0d83aca13233dd
sushantsy12sy12-ui <aceraspire5121@gmail.com>
2026-06-02 23:19:10 +0530

    working

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -4,14 +4,33 @@ import { User } from "../models/userModel.js";
 
 export const dashboardData=async (req,res)=>{
 try {
+    const sevendaysago=new Date()
+    sevendaysago.setDate(sevendaysago.getDate()-7)
     
-    const [totalOrders,totalUsers,recentOrders,lowStock,lowStockCount,recentUsers]=await Promise.all([
+    
+    const [totalOrders,totalUsers,recentOrders,lowStock,lowStockCount,recentUsers,usersthisweek,ordersthisweek,revenue]=await Promise.all([
          Order.countDocuments(),
          User.countDocuments({role:"user"}),
-         Order.find().sort({createdAt:-1}).limit(5).populate("user"),
+         Order.find({status:"paid"}).sort({createdAt:-1}).limit(5).populate("user"),
          Product.find({stock:{$lt:5}}).limit(5),
          Product.countDocuments({stock:{$lt:5}}),
          User.find({role:"user"}).sort({createdAt:-1}).limit(6),
+         User.countDocuments({role:"user",createdAt:{$gt:sevendaysago}}),
+         Order.countDocuments({status:"paid",createdAt:{$gt:sevendaysago}}),
+         Order.aggregate([
+          {
+            $facet:{
+                totalrevenue:[
+                    {$match:{status:"paid"}},
+                    {$group:{_id:null,amount:{$sum:"$amount"}}}
+                ],
+                weeklyrevenue:[
+                    {$match:{status:"paid",createdAt:{$gt:sevendaysago}}},
+                    {$group:{_id:null,amount:{$sum:"$amount"}}}
+                ]
+            }
+          }
+         ])
          
     ])
    const recent=await Promise.all(
@@ -25,7 +44,10 @@ try {
      }
     })
    )
-    res.status(200).json({success:true,totalOrders,totalUsers,recentOrders,lowStock,lowStockCount,recent})
+   console.log("revenue",revenue)
+   const totalrevenue=revenue[0]?.totalrevenue[0]?.amount || 0
+   const weeklyrevenue=revenue[0]?.weeklyrevenue[0]?.amount || 0
+    res.status(200).json({success:true,totalOrders,totalUsers,recentOrders,lowStock,lowStockCount,recent,usersthisweek,ordersthisweek,totalrevenue,weeklyrevenue})
 
 } catch (error) {
     console.log(error)
