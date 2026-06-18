382335fe298cc7f6045ffe6f6296c34436d49a7a
sushantsy12sy12-ui <aceraspire5121@gmail.com>
2026-06-18 21:38:35 +0530

    profile page

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,8 +1,8 @@
-// import express from "express"
-// import { verifyToken } from "../middleware/verifyToken.js";
-// import { getAllOrder, getOrder } from "../controllers/paymentController.js";
-// import { isadmin } from "../middleware/isadmin.js";
-// const router=express.Router();
-// router.get("/getOrders",verifyToken,getAllOrder) // always put constant bala uper and params bala neeche
-// router.get("/:id",verifyToken,getOrder)
-// export default router;
+import express from "express"
+import { verifyToken } from "../middleware/verifyToken.js";
+import { getAllOrder, getOrder } from "../controllers/paymentController.js";
+import { isadmin } from "../middleware/isadmin.js";
+const router=express.Router();
+router.get("/getOrders",verifyToken,getAllOrder) // always put constant bala uper and params bala neeche
+router.get("/:id",verifyToken,getOrder)
+export default router;
